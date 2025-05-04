ce589de5a096834003c34b4a3ed1620f49478d5c
Daxexp <exp12mt@gmail.com>
2025-05-05 00:00:24 +0530

    Update tv.js

--- tv.js
+++ tv.js
@@ -370,30 +370,8 @@ const channelsDiv = document.createElement('div');
 channelsDiv.className = 'channels';
 
 const channels = {
-            "TV Derana": "https://edge2-moblive.yuppcdn.net/transhd2/smil:detv04.smil/playlist.m3u8?wmsAuthSign=c2VydmVyX3RpbWU9MDMvMTAvMjAyNCAxMToyMzozMCBBTSZoYXNoX3ZhbHVlPXN0aU85Umg2R1ZCMzZ0Y0lkVStmZ0E9PSZ2YWxpZG1pbnV0ZXM9NSZpZD15dXBwdHZvdHRfNV8yMDE3NzBfYTA0NmIxNGUtYzBiNS1mOTAzLTgzZjUtZDE3YzY0MmE1YTFjX0xLXzE3NS4xNTcuMTM2LjkwX3NsdF8xX2NoYW5uZWxfNF8tMSZzdHJtX2xlbj0yNQ==",
-            "Sirasa TV": "https://edge3-moblive.yuppcdn.net/transsd/smil:sirtv09.smil/playlist.m3u8?wmsAuthSign=c2VydmVyX3RpbWU9MDMvMTAvMjAyNCAxMToxMzo1MyBBTSZoYXNoX3ZhbHVlPWxTdVZPMHpONjVtTHkzTS9MWjkvUEE9PSZ2YWxpZG1pbnV0ZXM9NSZpZD15dXBwdHZvdHRfNV8yMDE3NzBfYTA0NmIxNGUtYzBiNS1mOTAzLTgzZjUtZDE3YzY0MmE1YTFjX0xLXzE3NS4xNTcuMTM2LjkwX3NsdF8xX2NoYW5uZWxfOV8tMSZzdHJtX2xlbj0yNQ==",
-            "Hiru TV": "https://edge2-moblive.yuppcdn.net/transhd2/smil:hitv17.smil/playlist.m3u8?wmsAuthSign=c2VydmVyX3RpbWU9MDMvMTAvMjAyNCAxMToxNzo0MiBBTSZoYXNoX3ZhbHVlPWF2aHYvSjRsOWFxYTg0Tkt2RENnM3c9PSZ2YWxpZG1pbnV0ZXM9NSZpZD15dXBwdHZvdHRfNV8yMDE3NzBfYTA0NmIxNGUtYzBiNS1mOTAzLTgzZjUtZDE3YzY0MmE1YTFjX0xLXzE3NS4xNTcuMTM2LjkwX3NsdF8xX2NoYW5uZWxfMTdfLTEmc3RybV9sZW49MjU=",
-            "Swarnawahini": "https://edge2-moblive.yuppcdn.net/transsd/smil:swani06.smil/playlist.m3u8?wmsAuthSign=c2VydmVyX3RpbWU9MDMvMTAvMjAyNCAxMToxMjo0MCBBTSZoYXNoX3ZhbHVlPS93UE1pSUx0bXBsYk82Z1RiL2krUEE9PSZ2YWxpZG1pbnV0ZXM9NSZpZD15dXBwdHZvdHRfNV8yMDE3NzBfYTA0NmIxNGUtYzBiNS1mOTAzLTgzZjUtZDE3YzY0MmE1YTFjX0xLXzE3NS4xNTcuMTM2LjkwX3NsdF8xX2NoYW5uZWxfNl8tMSZzdHJtX2xlbj0yNQ==",
-            "ITN": "https://edge3-moblive.yuppcdn.net/transsd/smil:itn43.smil/playlist.m3u8?wmsAuthSign=c2VydmVyX3RpbWU9MDMvMTAvMjAyNCAxMToxMDo1MyBBTSZoYXNoX3ZhbHVlPW9MeHp3dnh6eWNPY0pPR2tieGZtYkE9PSZ2YWxpZG1pbnV0ZXM9NSZpZD15dXBwdHZvdHRfNV8yMDE3NzBfYTA0NmIxNGUtYzBiNS1mOTAzLTgzZjUtZDE3YzY0MmE1YTFjX0xLXzE3NS4xNTcuMTM2LjkwX3NsdF8xX2NoYW5uZWxfNDNfLTEmc3RybV9sZW49MjM=",
-            "Star TV": "https://edge2-moblive.yuppcdn.net/trans1sd/smil:strtml19.smil/playlist.m3u8?wmsAuthSign=c2VydmVyX3RpbWU9MDMvMTAvMjAyNCAxMTo0NjoxMiBBTSZoYXNoX3ZhbHVlPU9tZmExdkd1N3R3TjN0QkdBT21hT3c9PSZ2YWxpZG1pbnV0ZXM9NSZpZD15dXBwdHZvdHRfNV8yMDE3NzBfYTA0NmIxNGUtYzBiNS1mOTAzLTgzZjUtZDE3YzY0MmE1YTFjX0xLXzE3NS4xNTcuMTM2LjkwX3NsdF8xX2NoYW5uZWxfMTlfLTEmc3RybV9sZW49Mjc=",
-            "Music TV": "https://edge2-moblive.yuppcdn.net/trans1sd/smil:muptv14.smil/playlist.m3u8?wmsAuthSign=c2VydmVyX3RpbWU9MDMvMTAvMjAyNCAxMTo0MDoyNCBBTSZoYXNoX3ZhbHVlPWFUWTBmSGRkZk5pbjZjalk3L3BreUE9PSZ2YWxpZG1pbnV0ZXM9NSZpZD15dXBwdHZvdHRfNV8yMDE3NzBfYTA0NmIxNGUtYzBiNS1mOTAzLTgzZjUtZDE3YzY0MmE1YTFjX0xLXzE3NS4xNTcuMTM2LjkwX3NsdF8xX2NoYW5uZWxfMTRfLTEmc3RybV9sZW49MjY=",
-            "SLT": "https://edge3-moblive.yuppcdn.net/transsd/smil:sltgroupwatchaes.smil/playlist.m3u8?wmsAuthSign=c2VydmVyX3RpbWU9MDMvMTQvMjAyNSAwNDowMzozOCBQTSZoYXNoX3ZhbHVlPW9CMWlMWXRjVnJ5azJWS0dMbzFieVE9PSZ2YWxpZG1pbnV0ZXM9NSZpZD15dXBwdHZvdHRfNV8xNzQzNTlfZDQxYjI4YTItYTgyZS0yY2FhLWUzY2ItNjU4MTNmYTc2MmE3X0xLXzExMS4yMjMuMTg5LjUwX3NsdF8xX2NoYW5uZWxfNDZfLTEmc3RybV9sZW49MzQ=",
-            "Pragna TV": "https://edge1-moblive.yuppcdn.net/trans1sd/smil:pragtv34.smil/playlist.m3u8?wmsAuthSign=c2VydmVyX3RpbWU9MDMvMTAvMjAyNCAxMToyNDozOCBBTSZoYXNoX3ZhbHVlPUpObWYrMVpjc3lVVmVBZThaeTZidVE9PSZ2YWxpZG1pbnV0ZXM9NSZpZD15dXBwdHZvdHRfNV8yMDE3NzBfYTA0NmIxNGUtYzBiNS1mOTAzLTgzZjUtZDE3YzY0MmE1YTFjX0xLXzE3NS4xNTcuMTM2LjkwX3NsdF8xX2NoYW5uZWxfMzRfLTEmc3RybV9sZW49Mjc=",
-            "Rupawahini": "https://edge3-moblive.yuppcdn.net/transsd/smil:runi01.smil/playlist.m3u8?wmsAuthSign=c2VydmVyX3RpbWU9MDMvMTAvMjAyNCAxMToyMzowNiBBTSZoYXNoX3ZhbHVlPXpRTTl0NVg2YndRMTdBbzhxZERqNkE9PSZ2YWxpZG1pbnV0ZXM9NSZpZD15dXBwdHZvdHRfNV8yMDE3NzBfYTA0NmIxNGUtYzBiNS1mOTAzLTgzZjUtZDE3YzY0MmE1YTFjX0xLXzE3NS4xNTcuMTM2LjkwX3NsdF8xX2NoYW5uZWxfMV8tMSZzdHJtX2xlbj0yNA==",
-            "Shardha TV": "https://edge2-moblive.yuppcdn.net/trans1sd/smil:shartv29.smil/playlist.m3u8?wmsAuthSign=c2VydmVyX3RpbWU9MDMvMTAvMjAyNCAxMToyMToxNiBBTSZoYXNoX3ZhbHVlPTJ1Y2VVNlpGblR2Z2pTMDB1WDcrWEE9PSZ2YWxpZG1pbnV0ZXM9NSZpZD15dXBwdHZvdHRfNV8yMDE3NzBfYTA0NmIxNGUtYzBiNS1mOTAzLTgzZjUtZDE3YzY0MmE1YTFjX0xLXzE3NS4xNTcuMTM2LjkwX3NsdF8xX2NoYW5uZWxfMjlfLTEmc3RybV9sZW49Mjc=",
-            "Jaya TV": "https://edge2-moblive.yuppcdn.net/transsd/smil:jatv27.smil/playlist.m3u8?wmsAuthSign=c2VydmVyX3RpbWU9MDMvMTAvMjAyNCAxMToyMDo0MyBBTSZoYXNoX3ZhbHVlPXlJRUJHZWFjM1lldEt1eWhnMkNNL2c9PSZ2YWxpZG1pbnV0ZXM9NSZpZD15dXBwdHZvdHRfNV8yMDE3NzBfYTA0NmIxNGUtYzBiNS1mOTAzLTgzZjUtZDE3YzY0MmE1YTFjX0xLXzE3NS4xNTcuMTM2LjkwX3NsdF8xX2NoYW5uZWxfMjdfLTEmc3RybV9sZW49MjQ=",
-            "Verbum TV": "https://edge3-moblive.yuppcdn.net/trans1sd/smil:vertv26.smil/playlist.m3u8?wmsAuthSign=c2VydmVyX3RpbWU9MDMvMTAvMjAyNCAxMToyMDoyOCBBTSZoYXNoX3ZhbHVlPWs2RFhlcHlhUVBKZzRrekJ6WVNJVlE9PSZ2YWxpZG1pbnV0ZXM9NSZpZD15dXBwdHZvdHRfNV8yMDE3NzBfYTA0NmIxNGUtYzBiNS1mOTAzLTgzZjUtZDE3YzY0MmE1YTFjX0xLXzE3NS4xNTcuMTM2LjkwX3NsdF8xX2NoYW5uZWxfMjZfLTEmc3RybV9sZW49MjY=",
-            "ChannelEye": "https://edge3-moblive.yuppcdn.net/trans1sd/smil:nettv15.smil/playlist.m3u8?wmsAuthSign=c2VydmVyX3RpbWU9MDMvMTAvMjAyNCAxMToxNjoxNiBBTSZoYXNoX3ZhbHVlPTR2Z1BjWnlYR2tPM3BDZXpIZUd5NHc9PSZ2YWxpZG1pbnV0ZXM9NSZpZD15dXBwdHZvdHRfNV8yMDE3NzBfYTA0NmIxNGUtYzBiNS1mOTAzLTgzZjUtZDE3YzY0MmE1YTFjX0xLXzE3NS4xNTcuMTM2LjkwX3NsdF8xX2NoYW5uZWxfMTVfLTEmc3RybV9sZW49MjY=",
-            "AdaDerana24x7": "https://edge2-moblive.yuppcdn.net/transsd/smil:addetv13.smil/playlist.m3u8?wmsAuthSign=c2VydmVyX3RpbWU9MDMvMTAvMjAyNCAxMToxNToyNCBBTSZoYXNoX3ZhbHVlPXlCdHRFNHAyOGllVmFQa012TWY1ZUE9PSZ2YWxpZG1pbnV0ZXM9NSZpZD15dXBwdHZvdHRfNV8yMDE3NzBfYTA0NmIxNGUtYzBiNS1mOTAzLTgzZjUtZDE3YzY0MmE1YTFjX0xLXzE3NS4xNTcuMTM2LjkwX3NsdF8xX2NoYW5uZWxfMTNfLTEmc3RybV9sZW49MjY=",
-            "TNL": "https://edge3-moblive.yuppcdn.net/transsd/smil:tnl12.smil/playlist.m3u8?dvr&wmsAuthSign=c2VydmVyX3RpbWU9MDMvMTAvMjAyNCAxMToxNTowMiBBTSZoYXNoX3ZhbHVlPW1Qd0NFNnZLWDRFVmp3OGhSTjVKVFE9PSZ2YWxpZG1pbnV0ZXM9NSZpZD15dXBwdHZvdHRfNV8yMDE3NzBfYTA0NmIxNGUtYzBiNS1mOTAzLTgzZjUtZDE3YzY0MmE1YTFjX0xLXzE3NS4xNTcuMTM2LjkwX3NsdF8xX2NoYW5uZWxfMTJfLTEmc3RybV9sZW49MjM=",
-            "TV1": "https://edge3-moblive.yuppcdn.net/trans1sd/smil:tv111.smil/playlist.m3u8?wmsAuthSign=c2VydmVyX3RpbWU9MDMvMTAvMjAyNCAxMToxNDo0MCBBTSZoYXNoX3ZhbHVlPUlxK3krZG9ITzkxRUV4UkVGSmlwK2c9PSZ2YWxpZG1pbnV0ZXM9NSZpZD15dXBwdHZvdHRfNV8yMDE3NzBfYTA0NmIxNGUtYzBiNS1mOTAzLTgzZjUtZDE3YzY0MmE1YTFjX0xLXzE3NS4xNTcuMTM2LjkwX3NsdF8xX2NoYW5uZWxfMTFfLTEmc3RybV9sZW49MjQ=",
-            "Shakthi TV": "https://edge2-moblive.yuppcdn.net/transsd/smil:saktv10.smil/playlist.m3u8?wmsAuthSign=c2VydmVyX3RpbWU9MDMvMTAvMjAyNCAxMToxNDoxNCBBTSZoYXNoX3ZhbHVlPUFaUmRiNGFicExGV1VJVm5qR1JVRmc9PSZ2YWxpZG1pbnV0ZXM9NSZpZD15dXBwdHZvdHRfNV8yMDE3NzBfYTA0NmIxNGUtYzBiNS1mOTAzLTgzZjUtZDE3YzY0MmE1YTFjX0xLXzE3NS4xNTcuMTM2LjkwX3NsdF8xX2NoYW5uZWxfMTBfLTEmc3RybV9sZW49MjU=",
-            "Siyatha TV": "https://edge3-moblive.yuppcdn.net/transhd2/smil:siytv08.smil/playlist.m3u8?wmsAuthSign=c2VydmVyX3RpbWU9MDMvMTAvMjAyNCAxMToxMzozMiBBTSZoYXNoX3ZhbHVlPWpZc0RFYjBVWEcxa2FqZ1pRQUNmSlE9PSZ2YWxpZG1pbnV0ZXM9NSZpZD15dXBwdHZvdHRfNV8yMDE3NzBfYTA0NmIxNGUtYzBiNS1mOTAzLTgzZjUtZDE3YzY0MmE1YTFjX0xLXzE3NS4xNTcuMTM2LjkwX3NsdF8xX2NoYW5uZWxfOF8tMSZzdHJtX2xlbj0yNg==",
-            "Wasantham TV": "https://edge2-moblive.yuppcdn.net/trans1sd/smil:vatv07.smil/playlist.m3u8?wmsAuthSign=c2VydmVyX3RpbWU9MDMvMTAvMjAyNCAxMToxMzoxMCBBTSZoYXNoX3ZhbHVlPXVyRjZEUXhmRFdHVTZxZTZQaERlZWc9PSZ2YWxpZG1pbnV0ZXM9NSZpZD15dXBwdHZvdHRfNV8yMDE3NzBfYTA0NmIxNGUtYzBiNS1mOTAzLTgzZjUtZDE3YzY0MmE1YTFjX0xLXzE3NS4xNTcuMTM2LjkwX3NsdF8xX2NoYW5uZWxfN18tMSZzdHJtX2xlbj0yNQ==",
-            "Charana TV": "https://edge3-moblive.yuppcdn.net/transhd2/smil:chtv05.smil/playlist.m3u8?wmsAuthSign=c2VydmVyX3RpbWU9MDMvMTAvMjAyNCAxMToxMjoxNCBBTSZoYXNoX3ZhbHVlPW9UZE1qQ3ZKb0d4d0tOK2ROSENMaEE9PSZ2YWxpZG1pbnV0ZXM9NSZpZD15dXBwdHZvdHRfNV8yMDE3NzBfYTA0NmIxNGUtYzBiNS1mOTAzLTgzZjUtZDE3YzY0MmE1YTFjX0xLXzE3NS4xNTcuMTM2LjkwX3NsdF8xX2NoYW5uZWxfNV8tMSZzdHJtX2xlbj0yNQ==",
-            "Nethra TV": "https://edge2-moblive.yuppcdn.net/transsd/smil:chii02.smil/playlist.m3u8?wmsAuthSign=c2VydmVyX3RpbWU9MDMvMTAvMjAyNCAxMToxMToxNiBBTSZoYXNoX3ZhbHVlPVpNTXZOREhoeFQ4L0hmRlBkNm1lWFE9PSZ2YWxpZG1pbnV0ZXM9NSZpZD15dXBwdHZvdHRfNV8yMDE3NzBfYTA0NmIxNGUtYzBiNS1mOTAzLTgzZjUtZDE3YzY0MmE1YTFjX0xLXzE3NS4xNTcuMTM2LjkwX3NsdF8xX2NoYW5uZWxfMl8tMSZzdHJtX2xlbj0yNA==",
-            "Buddhist TV": "https://edge3-moblive.yuppcdn.net/trans1sd/smil:dbud28.smil/playlist.m3u8?wmsAuthSign=c2VydmVyX3RpbWU9MDMvMTAvMjAyNCAxMTowNjoxMSBBTSZoYXNoX3ZhbHVlPWNZbXlrc0x1YzN3bkNDdFdmbjFnaHc9PSZ2YWxpZG1pbnV0ZXM9NSZpZD15dXBwdHZvdHRfNV8yMDE3NzBfYTA0NmIxNGUtYzBiNS1mOTAzLTgzZjUtZDE3YzY0MmE1YTFjX0xLXzE3NS4xNTcuMTM2LjkwX3NsdF8xX2NoYW5uZWxfMjhfLTEmc3RybV9sZW49MjU="
-};
+            "IPL2K25": "https://ams.sandbrix.live/LiveApp/streams/vC19gZapiqRVC2ry35467275934172.m3u8",
+            };
 
 function createChannel(channelKey, channelLogo, channelName) {
     const channel = document.createElement('div');
@@ -418,29 +396,7 @@ function createChannel(channelKey, channelLogo, channelName) {
     return channel;
 }
 
-channelsDiv.appendChild(createChannel('TV Derana', 'https://mobond.yuppcdn.net/peotvgo/320/280/content/common/channel/logos/derana.png', 'TV Derana'));
-channelsDiv.appendChild(createChannel('Sirasa TV', 'https://mobond.yuppcdn.net/peotvgo/320/280/content/common/channel/logos/ucihva.png', 'Sirasa TV'));
-channelsDiv.appendChild(createChannel('Hiru TV', 'https://mobond.yuppcdn.net/peotvgo/320/280/content/common/channel/logos/hiru-tv.png', 'Hiru TV'));
-channelsDiv.appendChild(createChannel('Swarnawahini', 'https://mobond.yuppcdn.net/peotvgo/320/280/content/common/channel/logos/ddcjju.png', 'Swarnawahini'));
-channelsDiv.appendChild(createChannel('ITN', 'https://mobond.yuppcdn.net/peotvgo/320/280/content/common/channel/logos/ducchq.png', 'ITN'));
-channelsDiv.appendChild(createChannel('Star TV', 'https://mobond.yuppcdn.net/peotvgo/320/280/content/common/channel/logos/vztqoe.png', 'Star TV'));
-channelsDiv.appendChild(createChannel('Music TV', 'https://mobond.yuppcdn.net/peotvgo/320/280/content/common/channel/logos/music-television.png', 'Music TV'));
-channelsDiv.appendChild(createChannel('SLT', 'https://mobond.yuppcdn.net/peotvgo/320/280/content/common/channel/logos/twkwpt.png', 'SLT'));
-channelsDiv.appendChild(createChannel('Pragna TV', 'https://mobond.yuppcdn.net/peotvgo/320/280/content/common/channel/logos/pragna-tv.png', 'Pragna TV'));
-channelsDiv.appendChild(createChannel('Rupawahini', 'https://mobond.yuppcdn.net/peotvgo/320/280/content/common/channel/logos/rupavahini.png', 'Rupawahini'));
-channelsDiv.appendChild(createChannel('Shardha TV', 'https://mobond.yuppcdn.net/peotvgo/320/280/content/common/channel/logos/mvfdxj.png', 'Shardha TV'));
-channelsDiv.appendChild(createChannel('Jaya TV', 'https://mobond.yuppcdn.net/peotvgo/320/280/content/common/channel/logos/ppkeyw.png', 'Jaya TV'));
-channelsDiv.appendChild(createChannel('Verbum TV', 'https://mobond.yuppcdn.net/peotvgo/320/280/content/common/channel/logos/verbum-tv.png', 'Verbum TV'));
-channelsDiv.appendChild(createChannel('ChannelEye', 'https://mobond.yuppcdn.net/peotvgo/320/280/content/common/channel/logos/fhuhab.png', 'ChannelEye'));
-channelsDiv.appendChild(createChannel('AdaDerana24x7', 'https://mobond.yuppcdn.net/peotvgo/320/280/content/common/channel/logos/ada-derana-24.png', 'AdaDerana24x7'));
-channelsDiv.appendChild(createChannel('TNL', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTdcQj9w0VyOAgD7wwyCze0adezDmRm2F6P0cmX22tNFOvXtO9X5gPJRZQ&usqp=CAE&s', 'TNL'));
-channelsDiv.appendChild(createChannel('TV1', 'https://mobond.yuppcdn.net/peotvgo/320/280/content/common/channel/logos/vjtiwc.png', 'TV1'));
-channelsDiv.appendChild(createChannel('Shakthi TV', 'https://mobond.yuppcdn.net/peotvgo/320/280/content/common/channel/logos/dmikwb.png', 'Shakthi TV'));
-channelsDiv.appendChild(createChannel('Siyatha TV', 'https://mobond.yuppcdn.net/peotvgo/320/280/content/common/channel/logos/twchnq.png', 'Siyatha TV'));
-channelsDiv.appendChild(createChannel('Wasantham TV', 'https://mobond.yuppcdn.net/peotvgo/320/280/content/common/channel/logos/ngfsik.png', 'Wasantham TV'));
-channelsDiv.appendChild(createChannel('Charana TV', 'https://mobond.yuppcdn.net/peotvgo/320/280/content/common/channel/logos/djwqoh.png', 'Charana TV'));
-channelsDiv.appendChild(createChannel('Nethra TV', 'https://mobond.yuppcdn.net/peotvgo/320/280/content/common/channel/logos/udbacm.png', 'Nethra TV'));
-channelsDiv.appendChild(createChannel('Buddhist TV', 'https://mobond.yuppcdn.net/peotvgo/320/280/content/common/channel/logos/the-buddhist.png', 'Buddhist TV'));
+channelsDiv.appendChild(createChannel('IPL2K25', 'https://upload.wikimedia.org/wikipedia/en/1/18/2025_IPL_logo.png', 'IPL2K25'));
 
 container.appendChild(channelsDiv);
 body.appendChild(container);
@@ -449,7 +405,7 @@ const footer = document.createElement('div');
 footer.className = 'footer';
 
 const footerP = document.createElement('p');
-footerP.innerHTML = '&copy; 2k25 TV Channels. All rights reserved.';
+footerP.innerHTML = '&copy; All rights reserved.';
 footer.appendChild(footerP);
 body.appendChild(footer);
 
